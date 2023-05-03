ecddb17ff39440d105e250ada1191032580bea2b
Denis <ds19092004@gmail.com>
2023-05-03 11:08:17 +0200

    feat: do 6-lab functions

--- FirstLab.cpp
+++ FirstLab.cpp
@@ -1,89 +1,59 @@
 ﻿#include <iostream>
-#include <fstream>
-#include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-int main()
-{
-    setlocale(LC_ALL, "Russian");
-
-    // Открытие файла с текстом
-    ifstream file("text.txt");
-    if (!file.is_open())
-    {
-        cout << "Ошибка открытия файла!" << endl;
-        return 0;
+bool is_prime(int n) {
+    if (n < 2) {
+        return false;
     }
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
-    // Чтение текста из файла
-    string text;
-    getline(file, text, '\0');
-    file.close();
+struct Row {
+    int index;
+    int sum;
+};
 
-    // Запрос буквы
-    char letter;
-    cout << "Введите букву: ";
-    cin >> letter;
+bool compare_rows(const Row& r1, const Row& r2) {
+    return r1.sum < r2.sum;
+}
 
-    // Запрос количества слов
+int main() {
     int n;
-    cout << "Введите количество слов: ";
     cin >> n;
-
-    // Разбиение текста на слова
-    vector<string> words;
-    string word = "";
-    for (int i = 0; i < text.length(); i++)
-    {
-        if (isalpha(text[i]))
-        {
-            word += tolower(text[i]);
-        }
-        else if (word.length() > 0 && word[word.length() - 1] == letter)
-        {
-            words.push_back(word);
-            word = "";
-        }
-        else
-        {
-            word = "";
+    vector<vector<int>> matrix(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
         }
     }
-    if (word.length() > 0 && word[word.length() - 1] == letter)
-    {
-        words.push_back(word);
-    }
-
-    // Сортировка слов по длине в порядке не возрастания
-    sort(words.begin(), words.end(), [](const string& a, const string& b) { return a.length() > b.length(); });
-
-    // Вывод N самых длинных слов
-    ofstream output("output.txt");
-    if (!output.is_open())
-    {
-        cout << "Ошибка открытия файла!" << endl;
-        return 0;
-    }
-    output << "Список " << n << " самых длинных слов, оканчивающихся на букву " << letter << ":" << endl;
-    int count = 0;
-    for (string word : words)
-    {
-        if (word[word.length() - 1] == letter)
-        {
-            output << word << endl;
-            count++;
+    int col1 = -1, col2 = -1;
+    for (int j1 = 0; j1 < n - 1; j1++) {
+        for (int j2 = j1 + 1; j2 < n; j2++) {
+            bool found_prime = false;
+            for (int i = 0; i < n; i++) {
+                if (abs(matrix[i][j1]) == abs(matrix[i][j2]) && is_prime(abs(matrix[i][j1]))) {
+                    found_prime = true;
+                    break;
+                }
+            }
+            if (found_prime) {
+                col1 = j1;
+                col2 = j2;
+                break;
+            }
         }
-        if (count == n)
-        {
+        if (col1 != -1) {
             break;
         }
+
     }
-    output.close();
-
-    cout << "Готово!" << endl;
-
-    return 0;
 }
