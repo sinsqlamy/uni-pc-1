598a32e821168504f3a50c42ffc01bb128fb4fc1
Denis <ds19092004@gmail.com>
2023-05-03 10:44:01 +0200

    feat: 5-lab.1 string

---
+++ FirstLab.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype> // для функции isvowel и toupper
+
+using namespace std;
+
+bool isvowel(char c) { // функция проверки, является ли символ гласной буквой
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+        c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+}
+
+int main() {
+    string s;
+    getline(cin, s); // считываем строку с пробелами
+
+    for (int i = 0; i < s.length(); i++) {
+        if (isvowel(s[i])) { // если символ является гласной буквой
+            s[i] = toupper(s[i]); // делаем его заглавным
+        }
+    }
+
+    cout << s << endl; // выводим измененную строку
+
+    return 0;
+}
+
