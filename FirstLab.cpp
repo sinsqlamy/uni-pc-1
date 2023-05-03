7cb479314c5cd1b650ef0bca3d7f4c11a735156c
Denis <ds19092004@gmail.com>
2023-05-03 10:48:06 +0200

    feat: do 5.2-lab string

--- FirstLab.cpp
+++ FirstLab.cpp
@@ -1,26 +1,89 @@
 ﻿#include <iostream>
+#include <fstream>
 #include <string>
-#include <cctype> // для функции isvowel и toupper
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-bool isvowel(char c) { // функция проверки, является ли символ гласной буквой
-    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-        c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
 
-int main() {
-    string s;
-    getline(cin, s); // считываем строку с пробелами
-
-    for (int i = 0; i < s.length(); i++) {
-        if (isvowel(s[i])) { // если символ является гласной буквой
-            s[i] = toupper(s[i]); // делаем его заглавным
-        }
+    // Открытие файла с текстом
+    ifstream file("text.txt");
+    if (!file.is_open())
+    {
+        cout << "Ошибка открытия файла!" << endl;
+        return 0;
     }
 
-    cout << s << endl; // выводим измененную строку
+    // Чтение текста из файла
+    string text;
+    getline(file, text, '\0');
+    file.close();
+
+    // Запрос буквы
+    char letter;
+    cout << "Введите букву: ";
+    cin >> letter;
+
+    // Запрос количества слов
+    int n;
+    cout << "Введите количество слов: ";
+    cin >> n;
+
+    // Разбиение текста на слова
+    vector<string> words;
+    string word = "";
+    for (int i = 0; i < text.length(); i++)
+    {
+        if (isalpha(text[i]))
+        {
+            word += tolower(text[i]);
+        }
+        else if (word.length() > 0 && word[word.length() - 1] == letter)
+        {
+            words.push_back(word);
+            word = "";
+        }
+        else
+        {
+            word = "";
+        }
+    }
+    if (word.length() > 0 && word[word.length() - 1] == letter)
+    {
+        words.push_back(word);
+    }
+
+    // Сортировка слов по длине в порядке не возрастания
+    sort(words.begin(), words.end(), [](const string& a, const string& b) { return a.length() > b.length(); });
+
+    // Вывод N самых длинных слов
+    ofstream output("output.txt");
+    if (!output.is_open())
+    {
+        cout << "Ошибка открытия файла!" << endl;
+        return 0;
+    }
+    output << "Список " << n << " самых длинных слов, оканчивающихся на букву " << letter << ":" << endl;
+    int count = 0;
+    for (string word : words)
+    {
+        if (word[word.length() - 1] == letter)
+        {
+            output << word << endl;
+            count++;
+        }
+        if (count == n)
+        {
+            break;
+        }
+    }
+    output.close();
+
+    cout << "Готово!" << endl;
 
     return 0;
 }
-
